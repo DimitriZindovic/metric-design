ebe611156c2345ad54e35e69be858792aa66a909
Antoine Schmerber-Perraud <113776815+AntoineSP01@users.noreply.github.com>
2025-07-09 12:23:36 +0200

    Adds a circular progress indicator
    Implements a new component to display progress in a circular format.
    
    The App component now includes a ProgressCircular component and a button.
    A basic animation is added to the ProgressCircular.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from './components/Button';
+import ProgressCircular from './components/ProgressCircular/ProgressCircular';
 
 function App() {
-  return <Button onClick={() => alert('Button clicked!')}>Click Me!</Button>;
+  const [progress, setProgress] = useState<number>(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress(p => {
+        if (p >= 100) {
+          clearInterval(timer);
+          return 100;
+        }
+        return p + 1;
+      });
+    }, 50);
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <>
+      <Button onClick={() => alert('Button clicked!')}>Click Me!</Button>
+      <ProgressCircular
+        progress={progress}
+        size={150}
+        strokeWidth={12}
+        circleOneStroke='#ddd'
+        circleTwoStroke='#4caf50'
+        percentageFontSize={40}
+      />
+    </>
+  );
 }
 
 export default App;
