13d8bf2e7b39395801231fcf27316bc766f16432
Dimitri Zindovic <dimitri.zindovic2@gmail.com>
2025-07-09 11:57:00 +0200

    Renames and moves Button component
    Moves the Button component to a more conventional location.
    
    Renames the component file for better consistency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import React from "react";
-import Button from "./components/Button";
-
+import React from 'react';
+import Button from './components/Button';
 
 function App() {
-    return <Button onClick={() => alert("Button clicked!")}>
-      Click Me! 
-    </Button>;
+  return <Button onClick={() => alert('Button clicked!')}>Click Me!</Button>;
 }
 
 export default App;
